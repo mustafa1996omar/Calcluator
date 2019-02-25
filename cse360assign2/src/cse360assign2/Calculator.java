3b7c8672acd9abf5cb98348a651900004a2235f5
Mustafa Al Khalidi <mustafa996omar@gmail.com>
2019-02-25 14:23:45 -0700

    Calculator first version Documenting the file.

---
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -0,0 +1,74 @@
+/**
+ * Mustafa Al Khalidi, 306, assignment 2.
+ * Document your program using the following guidelines. 
+ * This program will be a calculator that will perform simple arithmetics.
+ * These arithmetics are add, subtract, multiply and divide.
+ * The program 
+ * The numbers will be sort in ascending order with no duplicates
+ * The array will be printed out in to two rows that each contain 5 integers 
+ */
+
+package cse360assign2;
+
+public class Calculator 
+{
+    private int total;
+     
+    // default constructor that will set total to zero
+    public Calculator () {
+            total = 0;  // not needed - included for clarity
+    }
+    
+    /**
+     * This method will return the total as an integer.
+     * @return 
+     */
+    public int getTotal () {
+            return 0;
+    }
+
+    /**
+     * This method will take an integer and add it to the total.
+     * The method does not return anything.
+     * @param value 
+     */
+    public void add (int value) {
+
+    }
+
+    /**
+     * This method will take an integer and subtract it from the total.
+     * The method does not return anything.
+     * @param value 
+     */
+    public void subtract (int value) {
+
+    }
+
+    /**
+     * This method will take an integer and multiply it to the total.
+     * The method does not return anything.
+     * @param value 
+     */
+    public void multiply (int value) {
+
+    }
+    
+    /**
+     * This method will take an integer and divide it from the total.
+     * The method does not return anything.
+     * @param value 
+    */
+    public void divide (int value) {
+
+    }
+
+    /**
+     * This method will return a string that contain the history of the
+     * calculator.
+     * @return 
+     */
+    public String getHistory () {
+            return "";
+    }
+}
